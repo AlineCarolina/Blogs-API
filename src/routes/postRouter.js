2188fa89f1a1a7102417fb64b3600bd0e75b475a
Aline <eualine2017@gmail.com>
2022-05-09 22:33:43 -0300

    feat: add routes

---
+++ src/routes/postRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { validateToken } = require('../../middlewares/validateToken');
+const { validateBlogPost } = require('../../middlewares/validateBlogPost');
+
+const blogPostController = require('../../controllers/blogPostController'); 
+
+const router = express.Router();
+
+router.post('/', validateToken, validateBlogPost, blogPostController.create);
+router.get('/', validateToken, blogPostController.getAll);
+
+module.exports = router;
